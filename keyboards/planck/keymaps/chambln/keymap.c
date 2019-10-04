cfac85c88d30e89411cbae270e964642eb8b17e8
Gregory Chamberlain <chambln@protonmail.com>
2019-10-04 03:07:32 +0100

    Add Tab, Bksp and Ret back to QWERTY layer.
    My muscle memory has adapted to using the Lower layer for these,
    but it's nice to have them on the unmodified layer too. In
    particular, Tab on the top-left-most key is useful for games.

--- keyboards/planck/keymaps/chambln/keymap.c
+++ keyboards/planck/keymaps/chambln/keymap.c
@@ -41,17 +41,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * |      |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |      |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '@  |
- * |  \|  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |  ,<  |  .>  |  /?  |      |
+ * |  \|  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |  ,<  |  .>  |  /?  | Ret  |
  * | Ctrl |      |      | GUI  |Lower |Space |Shift |Raise |   -  |      |      | Alt  |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______,
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______,
-    KC_LCTL, _______, _______, KC_LGUI, LOWER,   KC_SPC,  KC_LSFT, RAISE,   KC_MINS, _______, _______, KC_LALT
+    KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
+    KC_ESC , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+    KC_NUBS, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, _______,
+    KC_LCTL, _______, _______, KC_LGUI, LOWER  , KC_SPC , KC_LSFT, RAISE  , KC_MINS, _______, _______, KC_LALT
 ),
 
 /* Lower
