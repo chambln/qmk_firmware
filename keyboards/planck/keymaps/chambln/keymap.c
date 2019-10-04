756ba2eb32475cb3374130c1c654a2f138a812be
Gregory Chamberlain <chambln@protonmail.com>
2019-10-04 03:21:53 +0100

    Move volume controls to Lower layer J, K, and U.

--- keyboards/planck/keymaps/chambln/keymap.c
+++ keyboards/planck/keymaps/chambln/keymap.c
@@ -56,16 +56,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |      |      |      |  End |      |      |      |      |  Tab |      |  Up  |      |
- * |      | Home |      |  Del | Right|Escape|Backsp|      |      |      |      |      |
- * |      |      |  Dim |Bright|      | Left | Down | Enter| VolDn| VolUp| Mute |      |
+ * |      |      |      |  End |      |      |      | Mute |  Tab |      |  Up  |      |
+ * |      | Home |      |  Del | Right|Escape|Backsp| VolDn| VolUp|      |      |      |
+ * |      |      |  Dim |Bright|      | Left | Down | Enter|      |      |      |      |
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    _______, _______, _______, KC_END , _______, _______, _______, _______, KC_TAB , _______, KC_UP  , _______,
-    _______, KC_HOME, _______, KC_DEL , KC_RGHT, KC_ESC , KC_BSPC, _______, _______, _______, _______, _______,
-    _______, _______, KC_BRID, KC_BRIU, _______, KC_LEFT, KC_DOWN, KC_ENT , KC_VOLD, KC_VOLU, KC_MUTE, _______,
+    _______, _______, _______, KC_END , _______, _______, _______, KC_MUTE, KC_TAB , _______, KC_UP  , _______,
+    _______, KC_HOME, _______, KC_DEL , KC_RGHT, KC_ESC , KC_BSPC, KC_VOLD, KC_VOLU, _______, _______, _______,
+    _______, _______, KC_BRID, KC_BRIU, _______, KC_LEFT, KC_DOWN, KC_ENT , _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
     XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    EXT_PLV, XXXXXXX, XXXXXXX, KC_C,    KC_V,    XXXXXXX, XXXXXXX, KC_N,    KC_M,    XXXXXXX, XXXXXXX, KC_1
+    EXT_PLV, XXXXXXX, XXXXXXX, XXXXXXX, KC_C,    KC_V,    KC_N,    KC_M,    XXXXXXX, XXXXXXX, XXXXXXX, KC_1
 ),
 
 /* Adjust (Lower + Raise)
